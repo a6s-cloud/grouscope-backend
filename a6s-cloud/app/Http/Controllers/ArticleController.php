a1ab61474b6a370317a655fe80201745dea7de8d
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-02-23 23:09:35 +0900

    DB から情報を取ってきてレスポンスする処理を実装
    情報はまだ仮でテーブルの構成などは別途これから調整する

---
+++ a6s-cloud/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        return Article::all();
+    }
+}
