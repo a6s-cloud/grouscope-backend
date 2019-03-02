244ee66ff2e294c205cc99294ef8f93fb7fbadcf
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-03-02 18:47:55 +0900

    Added projects

---
+++ a6s-cloud/database/migrations/2019_03_02_074108_create_analysis_results_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnalysisResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('analysis_results', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('analysis_start_date');                            // 分析開始日付
+            // $table->dateTimeTz('analysis_start_date');
+            $table->dateTime('analysis_end_date');                              // 分析終了日付
+            $table->string('analysis_word', 255)->nullable()->charset('utf8');  // 分析ワード
+            $table->tinyInteger('status')->default(0);                          // ステータス
+            $table->integer('tweet_count')->nullable();                         // ツイート数
+            $table->integer('favorite_count')->nullable();                      // いいね数
+            $table->integer('user_count')->nullable();                          // ユーザ数
+            $table->binary('image')->nullable();                                // wordcloud 画像
+            $table->dateTime('insert_date')->userCurrent();                     // 追加日時
+            $table->dateTime('update_date')
+                  ->default(DB::raw('UPDATE CURRENT_TIMESTAMP'));               // 追加日時
+            $table->tinyInteger('delete_flag')->default(0);                     // 削除フラグ
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('analysis_results');
+    }
+}
