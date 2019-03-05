125ed4f28a4571107e08cb4d315b3b8b42133f51
Tsutomu Nakamura <tsuna.0x00@gmail>
2019-03-05 16:21:03 +0900

    インデントが崩れていたので修正

--- a6s-cloud/database/migrations/2019_03_02_074108_create_analysis_results_table.php
+++ a6s-cloud/database/migrations/2019_03_02_074108_create_analysis_results_table.php
@@ -15,19 +15,19 @@ class CreateAnalysisResultsTable extends Migration
     {
         Schema::create('analysis_results', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->dateTime('analysis_start_date');                            // 分析開始日付
+            $table->dateTime('analysis_start_date');                                    // 分析開始日付
             // $table->dateTimeTz('analysis_start_date');
-            $table->dateTime('analysis_end_date');                              // 分析終了日付
-            $table->string('analysis_word', 255)->nullable()->charset('utf8');  // 分析ワード
-            $table->tinyInteger('status')->default(0);                          // ステータス
-            $table->integer('tweet_count')->nullable();                         // ツイート数
-            $table->integer('favorite_count')->nullable();                      // いいね数
-            $table->integer('user_count')->nullable();                          // ユーザ数
-            $table->binary('image')->nullable();                                // wordcloud 画像
-            $table->dateTime('insert_date')->userCurrent();                     // 追加日時
+            $table->dateTime('analysis_end_date');                                      // 分析終了日付
+            $table->string('analysis_word', 255)->nullable()->charset('utf8');          // 分析ワード
+            $table->tinyInteger('status')->default(0);                                  // ステータス
+            $table->integer('tweet_count')->nullable();                                 // ツイート数
+            $table->integer('favorite_count')->nullable();                              // いいね数
+            $table->integer('user_count')->nullable();                                  // ユーザ数
+            $table->binary('image')->nullable();                                        // wordcloud 画像
+            $table->dateTime('insert_date')->useCurrent();                              // 追加日時
             $table->dateTime('update_date')
-                  ->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));               // 追加日時
-            $table->tinyInteger('delete_flag')->default(0);                     // 削除フラグ
+                  ->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));  // 追加日時
+            $table->tinyInteger('delete_flag')->default(0);                             // 削除フラグ
         });
     }
 
