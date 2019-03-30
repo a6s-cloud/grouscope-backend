c17eb71126ac38bb0a53a0dcaf7156f70ccc421b
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-03-31 00:38:23 +0900

    apiでデータ取得から保存

--- a6s-cloud/app/Http/Controllers/AnalysisRequestsController.php
+++ a6s-cloud/app/Http/Controllers/AnalysisRequestsController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\AnalysisResults;
+use App\Tweets;
 use Request;
+use Abraham\TwitterOAuth\TwitterOAuth;
 
 class AnalysisRequestsController extends Controller
 {
@@ -25,11 +27,53 @@ class AnalysisRequestsController extends Controller
         $results->save();
 
         // twitterデータ取得
+        $twitter_config = config('twitter');
+        $this->twitter_client = new TwitterOAuth(
+            $twitter_config["api_key"],
+            $twitter_config["secret_key"],
+            $twitter_config["access_token"],
+            $twitter_config["token_secret"]
+        );
+        // 認証要求したユーザーの情報を返す
+        // $content = $this->twitter_client->get("account/verify_credentials");
+        // logger(print_r($content, true));
+
+        // twitter serch
+        $params = ['q'=> '#phperkaigi',
+                   'count'=> 100,
+                   'result_type'=>'recent',
+                   'since'=>'2019-03-30_12:00:00_JST',
+                   'until'=>'2019-03-30_23:59:59_JST',
+                  ];
+        $statuses = $this->twitter_client->get("search/tweets", $params);
+
+        // $limit = $statuses->headers['x-rate-limit-remaining'];
+        logger(print_r($statuses,true));
+
+        // logger(print_r($statuses->statuses, true));
+        // analsis_resultsのデータを取得できる
+        // logger(print_r($results->id, true));
+
+        foreach($statuses->statuses as $key => $value){
+            // logger($key);
+            // logger(print_r($value,true));
+            $tweet = new Tweets;
+            $tweet->analysis_result_id = $results->id;
+            $tweet->user_name = $value->user->name;
+            $tweet->user_account = $value->user->screen_name;
+            $tweet->text = $value->text;
+            $tweet->retweet_count = $value->retweet_count;
+            $tweet->favorite_count = $value->favorite_count;
+            // $tweet->created_at = $value->created_at;
+            $tweet->created_at = date("Y/n/d H:i:s", strtotime($value->created_at));
+            // logger(print_r($tweet,true));
+            // $tweet->save();
+        }
 
         // 集計とかいろいろ
 
         // update処理
-        $results->status = 2;
-        $results->save();
+        // $results->status = 2;
+        // $results->save();
     }
 }
