28fae3eec1ef81b96a5d5429c81223abdaea5ac0
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-04-01 23:06:52 +0900

    大まかな処理を追加

--- a6s-cloud/app/Http/Controllers/AnalysisRequestsController.php
+++ a6s-cloud/app/Http/Controllers/AnalysisRequestsController.php
@@ -11,20 +11,20 @@ class AnalysisRequestsController extends Controller
 {
     public function create(Request $request)
     {
-        // bodyからパラメータを取得
+        // パラメータを取得
         $start_date = $request::input('start_date');
         $analysis_word = $request::input('analysis_word');
         $url = $request::input('url');
         $analysis_timing = $request::input('analysis_timing');
 
         // analysis_resultsにデータを保存
-        $results = new AnalysisResults;
-        $results->analysis_start_date = $start_date;
-        $results->analysis_end_date = $start_date;
-        $results->analysis_word = $analysis_word;
-        // $results->url = $url; TODO:カラムを追加する必要あり
-        $results->status = 1;
-        $results->save();
+        $aResult = new AnalysisResults;
+        $aResult->analysis_start_date = $start_date;
+        $aResult->analysis_end_date = $start_date;
+        $aResult->analysis_word = $analysis_word;
+        // $aResult->url = $url; TODO:カラムを追加する必要あり
+        $aResult->status = 1;
+        $aResult->save();
 
         // twitterデータ取得
         $twitter_config = config('twitter');
@@ -34,38 +34,28 @@ class AnalysisRequestsController extends Controller
             $twitter_config["access_token"],
             $twitter_config["token_secret"]
         );
-        // 認証要求したユーザーの情報を返す
-        // $content = $this->twitter_client->get("account/verify_credentials");
-        // logger(print_r($content, true));
 
         // twitter serch
-        $params = ['q'=> '#phperkaigi',
+        $params = ['q'=> $analysis_word,
                    'count'=> 100,
                    'result_type'=>'recent',
                    'since'=>'2019-03-30_12:00:00_JST',
                    'until'=>'2019-03-30_23:59:59_JST',
                   ];
-        $statuses = $this->twitter_client->get("search/tweets", $params);
+        $searchTweet = $this->twitter_client->get("search/tweets", $params);
+        // ツイートデータを確認
+        // logger(print_r($searchTweet->statuses, true));
 
-        // $limit = $statuses->headers['x-rate-limit-remaining'];
-        // logger(print_r($statuses,true));
-
-        // logger(print_r($statuses->statuses, true));
-        // analsis_resultsのデータを取得できる
-        // logger(print_r($results->id, true));
-
+        // サマリ件数を計算
         $total_retweet = 0;
         $total_favorite = 0;
         $total_tweet = 0;
 
+        // 暫定的に最大10回のリクエストをする(1000件取得)
         for ($i=0; $i<10; $i++) {
-            logger(print_r($i,true));
-
-            foreach($statuses->statuses as $key => $value){
-                // logger($key);
-                // logger(print_r($value,true));
+            foreach($searchTweet->statuses as $key => $value){
                 $tweet = new Tweets;
-                $tweet->analysis_result_id = $results->id;
+                $tweet->analysis_result_id = $aResult->id;
                 $tweet->user_name = $value->user->name;
                 $tweet->user_account = $value->user->screen_name;
                 $tweet->text = $value->text;
@@ -73,35 +63,40 @@ class AnalysisRequestsController extends Controller
                 $tweet->favorite_count = $value->favorite_count;
                 // $tweet->created_at = $value->created_at;
                 $tweet->created_at = date("Y/n/d H:i:s", strtotime($value->created_at));
-                // logger(print_r($value->id,true));
-                // $tweet->save();
+                $tweet->save();
 
+                // サマリを計算
                 $total_retweet = $total_retweet + $value->retweet_count;
                 $total_favorite = $total_favorite + $value->favorite_count;
                 $total_tweet = $total_tweet + 1;
-                // ユーザ数をカウントする処理
+                // ユーザ数をカウントする処理を追加する
+
+                // wordcloud用のテキストファイルを作成
             }
 
-            // 次のリクエストを投げる
+            // 次のリクエストを投げるためのpramerをセット
             $params["max_id"] = $value->id - 1;
-            // logger(print_r($params, true));
-            $statuses = $this->twitter_client->get("search/tweets", $params);
+
+            // 次のツイートデータを取得
+            $searchTweet = $this->twitter_client->get("search/tweets", $params);
 
             // 0件なら終了
-            $tweetCount = count($statuses->statuses);
-            // logger(print_r($tweetCount,true));
+            $tweetCount = count($searchTweet->statuses);
             if($tweetCount == 0){
                 break;
             }
         }
 
-        // 集計とかいろいろ
+        // wordcloudを実行
 
         // update処理
-        // $results->status = 2;
-        $results->favorite_count = $total_favorite;
-        $results->tweet_count = $total_tweet;
-        $results->favorite_count = $total_favorite;
-        $results->save();
+        $aResult->status = 2;
+        $aResult->favorite_count = $total_favorite;
+        $aResult->tweet_count = $total_tweet;
+        $aResult->favorite_count = $total_favorite;
+        $aResult->save();
+
+        // IDを取得を返す
+        return response($aResult->id, 200);
     }
 }
