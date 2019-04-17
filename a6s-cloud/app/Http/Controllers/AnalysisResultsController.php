fedeed0febd91295004b11422d021dbb9d6a30fc
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-04-18 01:09:55 +0900

    tweet_usersを追加

--- a6s-cloud/app/Http/Controllers/AnalysisResultsController.php
+++ a6s-cloud/app/Http/Controllers/AnalysisResultsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\AnalysisResults;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AnalysisResultsController extends Controller
 {
@@ -13,6 +14,16 @@ class AnalysisResultsController extends Controller
     }
     public function show($id)
     {
-        return AnalysisResults::find($id);
+        $results = AnalysisResults::find($id);
+
+        $tweet_users = DB::table('tweets')
+                        ->select(DB::raw('user_name,user_account,count(*) as tweet_count'))
+                        ->where('analysis_result_id', '=', $id)
+                        ->groupBy('user_name','user_account')
+                        ->orderBy('tweet_count', 'desc')
+                        ->get();
+
+        $results['user_ranking'] = $tweet_users;
+        return \Response::json($results);
     }
 }
