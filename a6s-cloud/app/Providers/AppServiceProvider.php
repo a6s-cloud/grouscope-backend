5705cc7978fec74c522fdd77ad1249c06d9087b6
TsutomuNakamura <tsuna.0x00@gmail.com>
2019-05-01 12:11:39 +0900

    Added an instruction to change default string length

--- a6s-cloud/app/Providers/AppServiceProvider.php
+++ a6s-cloud/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use App\Services\AnalysisRequestService;
 use App\Services\TwitterClientService;
@@ -27,6 +28,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
