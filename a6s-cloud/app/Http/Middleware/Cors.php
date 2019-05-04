91d7e23ffb71ad4f61e04337c2c63935cd2f300b
TsutomuNakamura <tsuna.0x00@gmail.com>
2019-05-04 13:01:34 +0900

    フロント側からのキャメルケースリクエストをスネークケースに変更する処理を追加

--- a6s-cloud/app/Http/Middleware/Cors.php
+++ a6s-cloud/app/Http/Middleware/Cors.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Middleware;
 use Closure;
+use Illuminate\Support\Str;
+
 class Cors
 {
     /**
@@ -12,6 +14,13 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
+        $requestParameters = $request->all();
+        $snakeParameters = array();
+        foreach($requestParameters as $key => $value) {
+            $snakeParameters[Str::snake($key)] = $value;
+        }
+        $request->replace($snakeParameters);
+
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
