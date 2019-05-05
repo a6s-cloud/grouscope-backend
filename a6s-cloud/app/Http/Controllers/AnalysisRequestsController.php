a3e9ca02b8be062e51c34229bd43db22e69b9eb3
TsutomuNakamura <tsuna.0x00@gmail.com>
2019-05-05 16:34:01 +0900

    Rule クラス名をより適切な名前へと変更

--- a6s-cloud/app/Http/Controllers/AnalysisRequestsController.php
+++ a6s-cloud/app/Http/Controllers/AnalysisRequestsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use AnalysisRequestService;
 use TwitterClientService;
-use App\Rules\NonFrequencyAnalysisRequest;
+use App\Rules\NonDuplicateAnalysisRequest;
 
 class AnalysisRequestsController extends Controller
 {
@@ -15,7 +15,7 @@ class AnalysisRequestsController extends Controller
         // バリデーション処理
         $request->validate([
             'start_date' => 'required',
-            'analysis_word' => ['required', new NonFrequencyAnalysisRequest($request)],
+            'analysis_word' => ['required', new NonDuplicateAnalysisRequest($request)],
             'analysis_timing' => 'required'
         ]);
 
