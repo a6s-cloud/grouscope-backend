0ebf40a65be0e796d3d95a541f1e2dec4ad586a5
TsutomuNakamura <tsuna.0x00@gmail.com>
2019-05-05 13:05:48 +0900

    同じハッシュタグ、同じ期間内の解析依頼制限を実装

--- a6s-cloud/app/Rules/NonFrequencyAnalysisRequest.php
+++ a6s-cloud/app/Rules/NonFrequencyAnalysisRequest.php
@@ -4,6 +4,9 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
+use \DateTime;
+use Illuminate\Support\Facades\DB;
+
 class NonFrequencyAnalysisRequest implements Rule
 {
     /**
@@ -25,8 +28,27 @@ class NonFrequencyAnalysisRequest implements Rule
      */
     public function passes($attribute, $value)
     {
-        // TODO: ここにバリデーション処理を追記する
-        return false;
+        // 過去の解析依頼の内、最も新しいものを取得する
+        $last_record = DB::table('analysis_results')
+            ->select('analysis_start_date')
+            ->where('analysis_word', '=', $value)
+            ->orderBy('analysis_start_date', 'desc')
+            ->take(1)
+            ->get()->shift();
+
+        if(!$last_record) {
+            // 今まで解析依頼したこと無いハッシュタグはパス
+            return true;
+        }
+
+        // 解析日時、現在日時をエポック秒に変換
+        $last_requested_date = $last_record->analysis_start_date;
+        $epoch_last_requested_date = strtotime($last_requested_date);
+        $epoch_current_date = time();
+        $current_date = (new DateTime("@$epoch_current_date"))->format('Y-m-d H:i:s');
+
+        // 30 分以内(30 * 60 * 1)の重複したリクエストは禁止する
+        return (($epoch_current_date - $epoch_last_requested_date) > (30 * 60 * 1));
     }
 
     /**
