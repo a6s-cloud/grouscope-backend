7e1e0c9c7a04e477fcd905bce424c910c8adb2df
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-05-09 21:15:34 +0900

    CSV からNG ワードのリストを読むように修正

--- a6s-cloud/database/seeds/BlackAnalysisWordsTableSeeder.php
+++ a6s-cloud/database/seeds/BlackAnalysisWordsTableSeeder.php
@@ -12,15 +12,12 @@ class BlackAnalysisWordsTableSeeder extends Seeder
      */
     public function run()
     {
-        // https://laravel.com/docs/5.8/eloquent#other-creation-methods
-        $word_list = array(
-                    array(0 => 'アイヌ系', 1 => 0),
-                    array(0 => '合いの子', 1 => 0)
-                );
-        foreach ($word_list as $key => $record) {
-            $match = BlackAnalysisWords::firstOrNew(array('analysis_ng_word' => $record[0]));
-            $match->availability_flag = $record[1];
-            $match->save();
+        if( ($handle = fopen(database_path() . '/csv/black_analysis_words.csv', 'r')) !== FALSE ) {
+            while (($record = fgetcsv($handle)) !== FALSE ) {
+                $match = BlackAnalysisWords::firstOrNew(array('analysis_ng_word' => $record[0]));
+                $match->availability_flag = $record[1];
+                $match->save();
+            }
         }
     }
 }
