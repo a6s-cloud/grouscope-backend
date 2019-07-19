87cd5923db97661752ce480cc55d236d2cc72125
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-07-20 00:42:26 +0900

    Updated

--- docker/laravel/grouscope_entrypoint.sh
+++ docker/laravel/grouscope_entrypoint.sh
@@ -1,6 +1,15 @@
 
 main() {
+    cd /var/www/html
+    composer install
+    if [[ ! -f .env ]]; then
+        cp -ip .env.example .env
+        php artisan key:generate
+    fi
 
+    # TODO:
+
+    true
 }
 
 main || exit 1
