71fce3bf417505c344f53a2d478749985b5d1b22
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-07-20 22:43:44 +0900

    Laravel マイグレーション処理を追加

--- docker/laravel/grouscope_entrypoint.sh
+++ docker/laravel/grouscope_entrypoint.sh
@@ -14,6 +14,9 @@ main() {
             cp -ip .env.example .env
             php artisan key:generate
         fi
+        [[ ! -L ./public/storage ]] && php artisan storage:link
+        php artisan migrate
+        php artisan db:seed
     '
 
     set -- php-fpm "$@"
