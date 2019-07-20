112f02b9e3cee0d52fa1b9fb94ba4e1e8536d647
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-07-21 01:43:38 +0900

    PHP 依存パッケージインストール後処理をcomposer.json 内へ移動

--- docker/laravel/grouscope_entrypoint.sh
+++ docker/laravel/grouscope_entrypoint.sh
@@ -3,25 +3,13 @@
 main() {
     set -e
 
+    cd /var/www/html/a6s-cloud
+    composer install
     chown -R www-data:www-data /var/www/html
-    su www-data -c '
-        set -e
-        cd /var/www/html/a6s-cloud
 
-        composer install
-
-        if [[ ! -f .env ]]; then
-            cp -ip .env.example .env
-            php artisan key:generate
-        fi
-        [[ ! -L ./public/storage ]] && php artisan storage:link
-        php artisan migrate
-        php artisan db:seed
-    '
-
-    set -- php-fpm "$@"
-    exec "$@"
+    exec php-fpm "$@"
 }
+
 main "$@"
 
 exit 0
